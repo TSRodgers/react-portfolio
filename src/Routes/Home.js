cd56f9c80e8e8814347a34ae47292d4786c9b540
TSRodgers <tyler.rodgers74@gmail.com>
2022-10-15 18:06:05 -0400

    setting up home page

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import Nav from '../components/Nav';
+import Header from '../components/Header';
+import HeroImg from '../assets/images/heroimg.jpg';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-      <Nav/>
+      <Header />
+      <div className='hero'>
+        <div className='mask'>
+          <img className='hero-img' src={HeroImg} alt='HeroImg' />
+        </div>
+        <div className='content'>
+          <p>Hi, I'm Tyler Rodgers</p>
+          <h1>Full-Stack Developer.</h1>
+          <div>
+            <Link to='/projects' className='btn'>Projects</Link>
+            <Link to='/contact' className='btn'>Contact</Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
