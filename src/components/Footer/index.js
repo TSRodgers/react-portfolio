2cbbc6d29b71deb6ae4a3c050c37c953aa0fe406
TSRodgers <tyler.rodgers74@gmail.com>
2022-10-16 14:07:06 -0400

    footer styling and added links to social icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,17 +6,30 @@ const Footer = () => {
   return (
     <div className='footer'>
       <div className='footer-container'>
-        <FaHome/>
-        <p>Raleigh, NC</p>
-        <FaPhone />
-        <p>1-919-413-3545</p>
-        <FaMailBulk />
-        <p>tyler.rodgers74@gmail.com</p>
-        <h4>About me</h4>
-        <p>My name is Tyler Rodgers, and I am a student attending the UNC Coding Bootcamp. This portfolio is a current work in progress, created for a class assignment.</p>
-        <FaGithub/>
-        <FaLinkedin/>
-        <FaStackOverflow />
+       <div className='contact'>
+          <div className='address'>
+            <FaHome size={20} style={{color: '#f0c808', marginRight: '2rem'}}/>
+            <p>Raleigh, NC</p>
+          </div>
+          <div className='phone'>
+            <FaPhone size={20} style={{color: '#f0c808', marginRight: '2rem'}}/>
+            1-919-413-3545
+          </div>
+          <div className='mail'>
+            <FaMailBulk size={20} style={{color: '#f0c808', marginRight: '2rem'}}/>
+            tyler.rodgers74@gmail.com
+          </div>
+       </div>
+        <div className='about'>
+          <h4>About me</h4>
+          <p>My name is Tyler Rodgers, and I am a student attending the UNC Coding Bootcamp.
+            This portfolio is a current work in progress, created for a class assignment.</p>
+          <div className='icons'>
+           <a href='https://github.com/TSRodgers'> <FaGithub size={30} style={{color: '#f0c808', marginRight: '1rem'}}/></a>
+           <a href='https://www.linkedin.com/in/tyler-rodgers-8a6259237/'> <FaLinkedin size={30} style={{color: '#f0c808', marginRight: '1rem'}}/></a>
+            <a href='https://stackoverflow.com/users/18741594/tyler-rodgers'><FaStackOverflow size={30} style={{color: '#f0c808', marginRight: '1rem'}}/></a>
+          </div>
+        </div>
       </div>
     </div>
   )
