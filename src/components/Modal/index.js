417c5c8b6eb677cf48a02ed658999ce33ac53d8f
TSRodgers <tyler.rodgers74@gmail.com>
2022-10-16 22:50:55 -0400

    added dynamic modal

---
+++ src/components/Modal/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FaDesktop, FaGithub } from 'react-icons/fa'
+
+const Modal = ({ onClose, currentProject }) => {
+  const {name, img, description, view, source} = currentProject;
+
+  return (
+    <div className='modalBackdrop'>
+      <div className='modalContainer'>
+        <h3 className='modalTitle'>{name}</h3>
+        <img src={img} alt={name} />
+        <p>
+          {description}
+        </p>
+        <div className='btn-container'>
+          <div className='close-btn'>
+            <button className='btn' onClick={onClose} type='button'>
+              Close
+            </button>
+          </div>
+          <div className='link-btn'>
+            <a href={view}><FaDesktop className='view-link'  size={40} style={{marginRight: '2rem'}}/></a>
+            <a href={source}><FaGithub className='source-link' size={40} style={{marginRight: '2rem'}}/></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Modal;
