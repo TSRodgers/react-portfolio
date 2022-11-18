245b182939b4b79a958b3429ecc413e619c1221b
TSRodgers <tyler.rodgers74@gmail.com>
2022-11-18 00:47:12 -0500

    deploying

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />,
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
