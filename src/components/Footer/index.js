a2f273bc74d6a658b6cf0b7502d5e2c953f3afdd
TSRodgers <tyler.rodgers74@gmail.com>
2022-11-17 21:26:06 -0500

    changed footer styles and added responsive css to projects

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import './index.css';
-import { FaGithub, FaHome, FaLinkedin, FaMailBulk, FaPhone, FaStackOverflow } from 'react-icons/fa'
+import { FaGithub, FaLinkedin, FaStackOverflow } from 'react-icons/fa'
 
 const Footer = () => {
   return (
     <div className='footer'>
       <div className='footer-container'>
-       <div className='contact'>
-          <div className='address'>
-            <FaHome size={20} style={{color: '#f0c808', marginRight: '2rem'}}/>
-            Raleigh, NC
-          </div>
-          <div className='phone'>
-            <FaPhone size={20} style={{color: '#f0c808', marginRight: '2rem'}}/>
-            1-919-413-3545
-          </div>
-          <div className='mail'>
-            <FaMailBulk size={20} style={{color: '#f0c808', marginRight: '2rem'}}/>
-            tyler.rodgers74@gmail.com
-          </div>
-       </div>
-        <div className='info'>
-          <h4>About me</h4>
-          <p>My name is Tyler Rodgers, and I am a student attending the UNC Coding Bootcamp.
-            This portfolio is a current work in progress, created for a class assignment.</p>
           <div className='icons'>
             <a className='icons-link' href='https://github.com/TSRodgers' target='_blank' rel='noreferrer'>
               <FaGithub size={30} style={{marginRight: '1rem'}}/>
@@ -35,9 +17,11 @@ const Footer = () => {
               <FaStackOverflow size={30} style={{marginRight: '1rem'}}/>
             </a>
           </div>
+          <div className='name'>
+            <p>Tyler Rodgers©</p>
+          </div>
         </div>
       </div>
-    </div>
   ) 
 }
 
